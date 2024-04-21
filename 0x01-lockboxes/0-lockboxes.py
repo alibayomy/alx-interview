fe4092d3b9c4133f24569f240cf4564f3fa3c183
alibayomy <alibayomy12@gmail.com>
2024-04-21 21:15:08 +0000

    adding conditions for edge cases

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,13 +9,15 @@ def canUnlockAll(boxes):
     boxes_map[0] = 1
 
     for key in boxes[0]:
-        boxes_map[key] = 1
+        if key < len(boxes):
+            boxes_map[key] = 1
 
     for index,box  in enumerate(boxes[1:]):
         looping_index =  index  + 1 
         if boxes_map[looping_index] == 1 or looping_index in box:
             for key in box:
-                boxes_map[key] = 1
+                if key < len(boxes):
+                    boxes_map[key] = 1
     for items in boxes_map:
         if items == 0:
             return False
