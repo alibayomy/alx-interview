013c9d6e9289b76319c216cce15d8857813ec77c
alibayomy <alibayomy12@gmail.com>
2024-04-21 21:03:40 +0000

    testing lockboxes answer

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+"""Solution for lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """Check if all the boxes can be oppend """
+
+    boxes_map = [0] * len(boxes)
+    boxes_map[0] = 1
+
+    for key in boxes[0]:
+        boxes_map[key] = 1
+
+    for index,box  in enumerate(boxes[1:]):
+        looping_index =  index  + 1 
+        if boxes_map[looping_index] == 1 or looping_index in box:
+            for key in box:
+                boxes_map[key] = 1
+    print(boxes_map)
+    for items in boxes_map:
+        if items == 0:
+            return False
+    return True
