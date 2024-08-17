931d304f6955fadb4f6d3a1ace1e1bf5ba4d1605
Ali Bayomy <alibayomy12@gmail.com>
2024-08-17 14:29:41 +0300

    testing answer

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def rotate_2d_matrix(matrix):
+    """Rotate a matrix 90 degrees clockwise"""
+    rotated_matrix = []
+    for row in range(len(matrix)):
+        rotatedRow= []
+        for column in range(len(matrix)-1 , -1, -1):
+            print(matrix[column][row])
+            rotatedRow.append(matrix[column][row])
+        rotated_matrix.append(rotatedRow)
+
+
